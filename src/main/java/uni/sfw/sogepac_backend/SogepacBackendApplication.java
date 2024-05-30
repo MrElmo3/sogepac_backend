307f3bcbdcf4fb7a4f71e2f63dddc3a9dcaa1407
Oscar Torres <oscar.torres.m@uni.pe>
2024-05-30 15:47:04 -0500

    first commit

---
+++ src/main/java/uni/sfw/sogepac_backend/SogepacBackendApplication.java
@@ -0,0 +1,13 @@
+package uni.sfw.sogepac_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SogepacBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SogepacBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uni/sfw/sogepac_backend/SogepacBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.sfw.sogepac_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SogepacBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
